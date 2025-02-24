4b46067fb5446c0fe5d9c55a33f60aea3cbb8aa9
Akila Dilshan Edirisooriya <edirisooriyaakila@gmail.com>
2025-02-24 23:28:48 +0530

    Modified Customer DashBoard

--- app/CustomerDashBoard/page.jsx
+++ app/CustomerDashBoard/page.jsx
@@ -1,7 +1,82 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import NewBooking from "../NewBooking/page";
 
 export default function CustomerDashBoard() {
+  const router = useRouter();
+  const handleLogout = () => {
+    router.push("/CustomerLogin");
+  };
+
+  //Use States
+  const [isClickedNewBook, setClickNewBook] = useState(false);
+
+
+  //Handling functions
+
+  const handleNewBookClick = ()=>{
+    if(isClickedNewBook == false){
+      setClickNewBook(true);
+    }else{
+      setClickNewBook(false);
+    }
+    
+  }
+
   return (
-    <div>CustomerDashBoard</div>
-  )
+    <div className="bg-slate-100 min-h-screen w-full">
+      <div className="bg-slate-600 h-[55px] flex flex-row items-center text-white shadow-md">
+        <div className="ml-4 hover:text-xl font-serif font-bold text-lg">
+          Mega City Dash Board
+        </div>
+        <div>
+          <input
+            className="border-none outline-none text-slate-500 px-2 w-[300px] rounded-sm ml-10"
+            type="text"
+            placeholder="Search Anything here"
+          />
+        </div>
+        <button
+          type="Submit"
+          className="border border-white text-white h-[26px] w-[75px] ml-2 hover:text-sm hover:shadow-md">
+          Search
+        </button>
+
+        <div className="ml-[180px] bg-slate-100 h-[35px] w-[500px] flex flex-row items-center justify-center">
+          <button
+          onClick={handleNewBookClick}
+            type="Submit"
+            className="border border-slate-600 text-slate-600 h-[26px] w-[110px] ml-2 hover:text-sm hover:shadow-md">
+            Book a Ride
+          </button>
+
+          <button
+            type="Submit"
+            className="border border-slate-600 text-slate-600 h-[26px] w-[125px] ml-2 hover:text-sm hover:shadow-md">
+            Cancel Booking
+          </button>
+
+          <button
+            type="Submit"
+            className="border border-slate-600 text-slate-600 h-[26px] w-[125px] ml-2 hover:text-sm hover:shadow-md">
+            Booking History
+          </button>
+
+          <button
+            onClick={handleLogout}
+            type="Submit"
+            className="border border-slate-600 text-slate-600 h-[26px] w-[90px] ml-2 hover:text-sm hover:shadow-md">
+            Log Out
+          </button>
+        </div>
+      </div>
+
+      {isClickedNewBook && (
+        <div className="mt-4">
+          <NewBooking />
+        </div>
+      )}
+    </div>
+  );
 }
---
+++ app/NewBooking/page.jsx
@@ -0,0 +1,112 @@
+"use client";
+import React, { useState } from "react";
+
+export default function NewBooking() {
+  //State Variables.
+  const [pickopLocation, setPickupLocation] = useState("");
+  const [destination, setDestination] = useState("");
+  const [vehicleType, setVehicleType] = useState("");
+
+  const [errorMsg, setErrorMsg] = useState("");
+
+  //handle functions.
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setErrorMsg("");
+    const textStates = [pickopLocation, destination, vehicleType];
+    textStates.forEach((element) => {
+      if (element == "") {
+        setErrorMsg("Please fill out all fields!");
+        return;
+      }
+    });
+
+    try {
+      const request = await fetch(
+        "http://localhost:8080/api/v1/booking/newBooking",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            pickupLocation: pickopLocation,
+            destination: destination,
+            vehicleType: vehicleType,
+          }),
+        }
+      );
+      if (request.ok) {
+        const response = await request.text();
+        alert(response);
+      } else {
+        alert("Error in response");
+      }
+    } catch (error) {
+        alert("Unable to send request");
+    }
+  };
+
+  return (
+    <div className="flex flex-col">
+      <div className="h-[35px] mt-2 flex flex-row items-center justify-center bg-blue-400 hover:bg-blue-500 text-white text-lg">
+        <label>Please add booking details</label>
+      </div>
+      <div className="h-[85px] bg-slate-50 shadow-md">
+        <form onSubmit={handleSubmit}>
+          <div className="flex flex-row mt-2">
+            <div className="mt-4 ml-2">
+              <label className="text-slate-600">Pickup Location: </label>
+              <input
+                onChange={(e) => setPickupLocation(e.target.value)}
+                type="text"
+                className="border border-slate-700 outline-blue-200 rounded-sm px-2 text-slate-500"
+                placeholder="Enter Pickup location"
+              />
+            </div>
+
+            <div className="mt-4 ml-[40px]">
+              <label className="text-slate-600">Destination: </label>
+              <input
+                onChange={(e) => setDestination(e.target.value)}
+                type="text"
+                className="border border-slate-700 outline-blue-200 rounded-sm px-2 text-slate-500"
+                placeholder="Enter Destination"
+              />
+            </div>
+
+            <div className="mt-4 ml-[40px]">
+              <label className="text-slate-600">Vehicle Type: </label>
+              <select
+                onChange={(e) => setVehicleType(e.target.value)}
+                type="text"
+                className="border border-slate-700 outline-blue-200 rounded-sm px-2 text-slate-500 w-[200px] h-[26px]"
+                placeholder="Enter Destination">
+                <option></option>
+                <option>Car</option>
+                <option>Mini Cab</option>
+                <option>Van</option>
+                <option>Bike</option>
+                <option>Tuk Tuk</option>
+                <option>Lorry</option>
+                <option>Bus</option>
+              </select>
+            </div>
+
+            <div className="mt-[15px] ml-4">
+              <button
+                type="submit"
+                className="border border-slate-600 hover:text-white hover:bg-slate-600 w-[70px] h-[28px]">
+                Submit
+              </button>
+            </div>
+          </div>
+          {errorMsg && (
+            <div className="mt-2 flex flex-col items-center text-red-500 text-sm">
+              <label>Error Message</label>
+            </div>
+          )}
+        </form>
+      </div>
+    </div>
+  );
+}
